d3ed15235c6bc1291ebb3f2b9f75413be2e1a738
wfilipunas <wfilipunas@gmail.com>
2021-05-16 21:09:07 +0200

    suma zmiennych

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let firstNumber = 5;
+let secondNumber = 3;
+
+function sum(firstNumber,secondNumber){
+    let result = firstNumber + secondNumber
+
+    return result
+}
+
+let wynikDodawania = sum(firstNumber,secondNumber);
+
+console.log(wynikDodawania);
